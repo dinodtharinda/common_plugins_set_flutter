0ae697262165c914651bbba7c1f7c9fb1b3a5cd3
dinod <dinodtharinda2001@gmail.com>
2026-06-05 08:16:22 +0530

    initial commit

--- android/src/main/kotlin/com/example/common_plugin_set/CommonPluginSetPlugin.kt
+++ android/src/main/kotlin/com/example/common_plugin_set/CommonPluginSetPlugin.kt
@@ -1,6 +1,6 @@
 package com.example.common_plugin_set
 
-
+import android.content.Context
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
@@ -8,15 +8,20 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-
 /** CommonPluginSetPlugin */
 class CommonPluginSetPlugin :
     FlutterPlugin,
     MethodCallHandler {
+    // The MethodChannel that will the communication between Flutter and native Android
+    //
+    // This local reference serves to register the plugin with the Flutter Engine and unregister it
+    // when the Flutter Engine is detached from the Activity
     private lateinit var channel: MethodChannel
-
+    private lateinit var context: Context
 
     override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        context = flutterPluginBinding.applicationContext
+
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "common_plugin_set")
         channel.setMethodCallHandler(this)
     }
