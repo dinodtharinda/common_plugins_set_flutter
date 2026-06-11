7d6fbee84f8ba2cde66273bc953076813a063ba3
dinod <dinodtharinda2001@gmail.com>
2026-06-11 09:14:52 +0530

    Add makePayment fucntion on IOS platform

--- ios/Classes/CommonPluginSetPlugin.swift
+++ ios/Classes/CommonPluginSetPlugin.swift
@@ -1,19 +1,29 @@
 import Flutter
 import UIKit
 
-public class CommonPluginSetPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "common_plugin_set", binaryMessenger: registrar.messenger())
-    let instance = CommonPluginSetPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+public class CommonPluginSetPlugin: NSObject, FlutterPlugin, PaymentGateway {
+    
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        
+        let instance = CommonPluginSetPlugin()
+        let messanger =  registrar.messenger()
+        
+        PaymentGatewaySetup.setUp(binaryMessenger: messanger, api: instance)
+        
     }
-  }
+    
+    func initialize(config: PaymentConfig, completion: @escaping (Result<Void, any Error>) -> Void) {
+        
+    }
+    
+    func makePayment(request: PaymentRequest, completion: @escaping (Result<PaymentResult, any Error>) -> Void) {
+        Task.detached{
+            try await Task.sleep(nanoseconds: 2_000_000_000)
+            completion(Result.success(PaymentResult(title: "success", result: "\(request.transactionId) Payment successful \(request.amount)\(request.currency)!")))
+        }
+        
+    }
+    
+    
 }
